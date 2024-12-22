c1b1501d59f7a516394e997d9d344b295d88203a
Mohammed Al <fire.senfony@gmail.com>
2024-12-22 04:11:00 -0500

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -3,10 +3,9 @@
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
+#include <unistd.h> // for close
 
 int main(){
-	char server_mssg[256] = "Hooo I am the server mssg, hooo I came form underground";
-	
 	//create the server socket
 	int server_socket;
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -17,19 +16,39 @@ int main(){
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = INADDR_ANY; // this const resolves to any IP address on the local machine
 	
-	// bind the socket to the specified IP and port, ie listen function
+	// bind the socket to the specified IP and port, and listen for clients
 	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-	listen(server_socket, 5); //arbitary number in our case, ciz I dont plan on hving big traffic
+	listen(server_socket, 5); //5 is an arbitary number
 	
+	// the client socket is different from the listening socket.
+	// the listening socket takes connections from clients and 
+	// after a connection is accepted, that client talks with the server
+	// with the the accept socket and keeps the listen socket free for other 
+	// clients to connect.
 	int client_socket;
 	client_socket = accept(server_socket, NULL, NULL);
-	
-	send(client_socket, server_mssg, sizeof(server_mssg),0); //send mssg
+	int msg = 1;
+    while(1){
+        printf("sending %d\n", msg);
+        char server_mssg[256];
+        char clientreply[256];
+        sprintf(server_mssg, "%d", msg);
+    	send(client_socket, server_mssg, sizeof(server_mssg),0); 
+        msg = msg*9;
+
+	int bytesrec = recv(client_socket, &clientreply, sizeof(clientreply),0);
+	if (bytesrec == -1){
+		printf("error receiving from client\n");
+	}
+	else {
+		printf("%s\n", clientreply);
+	}
+
+        sleep(1);
+    }
 
 	close(server_socket);	
 
 	return 0;
 	
 }
-
